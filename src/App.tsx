e0984c2f27de42d8646824f8ba5296a6799fcf16
Agnes Kyuun Park <agnespark0413@gmail.com>
2023-09-22 19:08:16 +0900

    Revert "feat: 프로젝트 구조 세팅, 메인 (홈) 작업"

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import Router from "./components/Router";
+import "./App.css";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Post</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<h1>homepage</h1>} />
+        <Route path="/posts" element={<h1>post list page</h1>} />
+        <Route path="/posts/:id" element={<h1>post detail page</h1>} />
+        <Route path="/posts/new" element={<h1>post new page</h1>} />
+        <Route path="/posts/edit" element={<h1>post edit page</h1>} />
+        <Route path="/profile" element={<h1>profile page</h1>} />
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
