52ae840208b6d248087339abe69c410d2a33cb73
Agnes Kyuun Park <agnespark0413@gmail.com>
2023-09-22 22:32:55 +0900

    feat: 재세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import "./App.css";
-import { Route, Routes, Navigate, Link } from "react-router-dom";
+import Router from "./components/Router";
 
 function App() {
-  return (
-    <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Post</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<h1>homepage</h1>} />
-        <Route path="/posts" element={<h1>post list page</h1>} />
-        <Route path="/posts/:id" element={<h1>post detail page</h1>} />
-        <Route path="/posts/new" element={<h1>post new page</h1>} />
-        <Route path="/posts/edit" element={<h1>post edit page</h1>} />
-        <Route path="/profile" element={<h1>profile page</h1>} />
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
