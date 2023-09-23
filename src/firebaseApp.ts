6334cd9857697018c673df00a888084455703df6
Agnes Kyuun Park <agnespark0413@gmail.com>
2023-09-23 19:25:07 +0900

    feat: firebase auth 세팅

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,4 +1,7 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import "firebase/auth";
+
+export let app: FirebaseApp;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -9,6 +12,13 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
 };
+
+// import시 getApp을 하고, 그기 아니면 초기화시키기. import 할때마다 초기화시키는게 비효율적이기 때문
+try {
+  app = getApp("app");
+} catch (e) {
+  app = initializeApp(firebaseConfig, "app");
+}
 
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
